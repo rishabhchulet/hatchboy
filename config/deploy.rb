9c61ebe09de9eda8403fd981a09651f0b793c15e
Andrey Glushkov <aglushkov@shakuro.com>
2013-11-25 13:17:21 +0200

    deploy test

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,16 +2,22 @@ set :application, 'hatchboy'
 set :user, 'hatchboy'
 
 set :stages, %w(staging production)
+# set :default_env, { path: '$HOME/.rvm/bin:$PATH' }
 
 set :repo_url, 'git@github.com:kstepanov/hatchboy.git'
 set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
-# set :default_stage, 'staging'
-# set :pty, true
+set :pty, true
+set :forward_agent, true
 
 set :linked_files, %w{config/database.yml .env}
 set :keep_releases, 3
 
-namespace :deploy do
-  after :finishing, 'deploy:cleanup'
-end
+# set :bundle_gemfile, -> { release_path.join('Gemfile') }
+# set :bundle_dir, -> { shared_path.join('bundle') }
+# set :bundle_flags, '--deployment --quiet'
+# set :bundle_without, %w{test}.join(' ')
+# set :bundle_binstubs, -> { shared_path.join('bin') }
+# set :bundle_roles, :all
+set :bundle_bins, %w(gem rake ruby foreman)
+
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,17 +2,30 @@ set :stage, :staging
 set :deploy_to, "/var/www/projects/hatchboy/#{fetch(:stage)}"
 set :upstart_script, "#{fetch(:application)}-#{fetch(:stage)}"
 
+set :rvm_type, :user
+
 server 'shakuro.com', user: fetch(:user), roles: %w{web app db}
 
 namespace :deploy do
   desc 'Restart application'
   task :restart do
-    on roles(:web), in: :sequence, wait: 5 do
-      execute <<-FOREMAN
-        cd #{release_path}
-        /home/hatchboy/.rvm/bin/rvmsudo bundle exec foreman export upstart /etc/init -a #{fetch(:upstart_script)} -u #{fetch(:user)} -l /var/log/#{fetch(:upstart_script)}
-        sudo start #{fetch(:upstart_script)} || sudo restart #{fetch(:upstart_script)}
-      FOREMAN
-    end
+    # on roles(:web), in: :sequence, wait: 5 do
+    #   within release_path do
+        invoke :'rvm:trust_rvmrc'
+        invoke :'foreman:export'
+        invoke :'foreman:restart'
+        # execute :cd, release_path
+        # execute :pwd, release_path
+        # execute "~/.rvm/bin/rvm rvmrc trust #{release_path}"
+        # # execute :cat, 'Gemfile'
+        # execute "cd #{release_path} && ~/.rvm/bin/rvmsudo bundle exec foreman export upstart /etc/init -a #{fetch(:upstart_script)} -u #{fetch(:user)} -l /var/log/#{fetch(:upstart_script)}"
+        # execute "sudo start #{fetch(:upstart_script)} || sudo restart #{fetch(:upstart_script)}"
+        # execute <<-FOREMAN
+        #   ~/.rvm/bin/rvm rvmrc trust #{release_path}
+        #   sudo bundle exec foreman export upstart /etc/init -a #{fetch(:upstart_script)} -u #{fetch(:user)} -l /var/log/#{fetch(:upstart_script)}
+        #   sudo start #{fetch(:upstart_script)} || sudo restart #{fetch(:upstart_script)}
+        # FOREMAN
+    #   end
+    # end
   end
 end
