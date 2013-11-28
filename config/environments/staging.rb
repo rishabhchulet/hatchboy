d2eaca92bc6919a8410af13b558b342392f17578
Alexey Bezruchko <abezruchko@shakuro.com>
2013-11-28 17:21:37 +0200

    Task #14: Setup mail enviroment for staging

---
+++ config/environments/staging.rb
@@ -0,0 +1,6 @@
+Hatchboy::Application.configure do
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  config.action_mailer.default_url_options = { :host => 'hatchboy.shakuro.com' }
+
+end
