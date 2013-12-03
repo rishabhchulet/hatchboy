8547561ecfc62f7012a5540557335bf5469c6fd8
Alexey Bezruchko <abezruchko@shakuro.com>
2013-12-03 10:08:10 +0200

    Task #5: Apply new theme

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,11 @@
 class PagesController < ApplicationController
   
   def home
+    redirect_to account_dashboard_path if account_signed_in?
+  end
+  
+  def dashboard
+    redirect_to root_path unless account_signed_in?
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Hatchboy::Application.routes.draw do
   root :to => "pages#home"
   devise_for :accounts, :controllers => {:registrations => "registrations"}
   resources :accounts
+  
+  get "dashboard", :to => "pages#dashboard", :as => :account_dashboard
+  get "company", :to => "companies#show"
+  resources :companies, :only => [:edit, :update]
+  
 end
