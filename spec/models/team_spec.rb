4bd43e13d6a53f2e610268488b23973f63965688
Alexey Bezruchko <abezruchko@shakuro.com>
2014-01-09 22:03:57 +0200

    Task #26: Remove source-team link when team removed

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -12,5 +12,12 @@ describe Team do
     worklogs = create_list :work_log, 10, team: team
     expect{team.destroy}.to change{WorkLog.count}.from(10).to(0)
   end
+
+  it "should remove links to all associated sources" do
+    team = create :team
+    source = create :authorized_jira_source
+    team.sources << source
+    expect{team.destroy}.to change{source.reload.source_teams.count}.from(1).to(0)
+  end
   
 end
