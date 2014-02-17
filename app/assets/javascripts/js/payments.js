1ce404f642663067555ee993e8e38fb9310e143e
Andrey Glushkov <aglushkov@shakuro.com>
2014-02-17 19:14:25 +0200

    Task #34. Payments controller and views

---
+++ app/assets/javascripts/js/payments.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+  $('.payment .amount').on('keyup change', function(){
+    var val = $(this).val();
+    if (val != '0' && val != '') {
+      checkRecipient($(this), true);
+    } else {
+      checkRecipient($(this), false);
+    }
+  });
+
+  $('.payment .check').on('change', function(){
+    checkRecipient($(this), $(this).prop("checked"));
+  });
+
+  $('form.payment').submit(function(e){
+    $(this).find("input.check").not(':checked').each(function(){
+      $(this).closest('tr').find('input').prop('disabled', true);
+    });
+  });
+});
+
+function checkRecipient(obj, to_check) {
+  var line = obj.closest('tr');
+  var checkbox = line.find(':checkbox');
+
+  checkbox.prop("checked", to_check);
+  if (to_check) {
+    line.addClass('row_selected');
+  } else {
+    line.removeClass('row_selected');
+  }
+};
---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,71 @@
+class PaymentsController < ApplicationController
+
+  before_filter :authenticate_account!
+  
+  def new 
+    @payment = Payment.new
+    @users = users_without_recipients
+  end
+
+  def index
+    @payments_prepared = account_company.payments.includes({recipients: :user}).where(status: Payment::STATUS_PREPARED).where.not(deleted: true).order(created_at: :desc)
+    @payments_sent = account_company.payments.includes({recipients: :user}, :transaction).where(status: Payment::STATUS_SENT).where.not(deleted: true).order(created_at: :desc)
+    @payments_deleted = account_company.payments.includes({recipients: :user}, :transaction).where(deleted: true).order(created_at: :desc)
+  end
+
+  def create
+    @payment = account_company.payments.new(payment_params)
+    @payment.created_by = current_account.user
+    if @payment.save
+      redirect_to :payments
+    else
+      @users = users_without_recipients
+      render :new        
+    end  
+  end
+
+  def show
+    @payment = account_company.payments.where(id: params[:id]).first or not_found
+  end
+
+  def edit
+    @payment = account_company.payments.where(id: params[:id]).where.not(deleted: true).first or not_found
+    @users = users_without_recipients
+  end
+
+  def update
+    @payment = account_company.payments.where(id: params[:id]).where.not(deleted: true).first or not_found
+    @payment.created_by = current_account.user
+    Payment.transaction do
+      @payment.recipients.destroy_all if payment_params.has_key?(:recipients_attributes)
+      @payment.assign_attributes(payment_params)
+      @payment.save
+    end
+
+    unless @payment.errors.any?
+      redirect_to :payments
+    else
+      @users = users_without_recipients
+      render :edit
+    end  
+  end
+
+  def destroy
+    @payment = account_company.payments.where(id: params[:id]).where.not(deleted: true).first or not_found
+    @payment.deleted = true
+    @payment.save!
+
+    redirect_to :payments
+  end
+  
+  private
+  
+  def payment_params
+    params.require(:payment).permit(:description, recipients_attributes: [:user_id, :amount])
+  end
+
+  def users_without_recipients
+    account_company.users - @payment.recipients.map(&:user)
+  end
+
+end
