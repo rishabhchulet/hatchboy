e1fa1e47730d6e9e26201700105ea3e69dfcfc4f
Andrey Glushkov <aglushkov@shakuro.com>
2014-02-24 13:23:02 +0200

    Task #34. Tests little improved

---
+++ spec/lib/hatchboy/payments/paypal_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Hatchboy::Payments::Paypal do
+  it "should create mass_pay"
+  it "should get mass pays transactions info"
+  it "should check payment status"
+end
