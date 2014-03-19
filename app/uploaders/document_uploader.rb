4fe21bf66b90106c74a96f0941d23236b15c9c24
Povstyanoy <apovstyanoy@shakuro.com>
2014-03-19 11:24:06 +0200

    uploader

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -4,8 +4,6 @@ require 'carrierwave/processing/mime_types'
 class DocumentUploader < CarrierWave::Uploader::Base
   #include CarrierWave::MimeTypes
 
-  process :set_envelope_id
-
   def extension_white_list
     %w(pdf)
   end
@@ -17,32 +15,5 @@ class DocumentUploader < CarrierWave::Uploader::Base
   def root
     Rails.root.to_s + '/public/'
   end
-
-  def set_envelope_id
-    client = DocusignRest::Client.new
-    response = client.create_envelope_from_document(
-      email: {
-        subject: 'Sign this document PLEASE!',
-        body: 'This is the email body.'
-      },
-      signers: [
-        {
-          embedded: true,
-          name: model.user.name,
-          email: model.user.account.email
-        }
-      ],
-      files: [
-        {path: file.path.to_s, name: file.filename}
-      ],
-      status: 'sent'
-    )    
-
-    raise CarrierWave::ProcessingError, response["message"] if response["errorCode"]
-
-    model.envelope_id = response["envelopeId"]
-    model.status = DocuSign::STATUS_PROCESSING
-  end
-
 
 end
