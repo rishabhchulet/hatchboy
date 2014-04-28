865c717c5c1338a57b646011d33a3d745d2cb733
aborodavchenko <aborodavchenko@shakuro.com>
2014-04-28 11:21:08 +0400

    Fixed bugs in templates

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,7 +9,7 @@ class ReportsController < ApplicationController
     @payments_report = Hatchboy::Reports::Builders::PaymentsBuilder.new(retrieve_query_params(:payments, Hatchboy::Reports::Builders::PaymentsBuilder::AVAILABLE_PARAMS))
     @mvp_report = Hatchboy::Reports::Builders::MvpBuilder.new(retrieve_query_params(:mvp, Hatchboy::Reports::Builders::MvpBuilder::AVAILABLE_PARAMS))
     @ratings_report = Hatchboy::Reports::Builders::RatingsBuilder.new({})
-
+    
   rescue Exception => e
     flash.now[:error] = e.message
     render :index
