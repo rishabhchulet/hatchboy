08e89e0c74900654cf5394d8fd2108eb6381a86c
aborodavchenko <aborodavchenko@shakuro.com>
2014-06-10 17:32:12 +0400

    Task #15 - Dashboard (Fixed bug)

--- app/helpers/work_logs_helper.rb
+++ app/helpers/work_logs_helper.rb
@@ -2,7 +2,7 @@ module WorkLogsHelper
 
   def company_work_logs company, in_date = nil
     worklogs = WorkLog.where(user_id: company.users)
-    worklogs = worklogs.where(created_at: in_date) if in_date
+    worklogs = worklogs.where(on_date: in_date) if in_date
     worklogs.sum(:time)
   end
 end
