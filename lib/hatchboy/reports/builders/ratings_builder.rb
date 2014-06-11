736fac58a68a42bfbdae5dfc699063f934cfd0cf
aborodavchenko <aborodavchenko@shakuro.com>
2014-06-11 15:20:37 +0400

    Task #15 - Changed in layout and ratings report

--- lib/hatchboy/reports/builders/ratings_builder.rb
+++ lib/hatchboy/reports/builders/ratings_builder.rb
@@ -7,6 +7,8 @@ module Hatchboy
         
         attr_reader :users
 
+        AVAILABLE_PARAMS = [:date, :period_from, :period_to]
+
         def build_report_data params = {}
           @users = @company ? @company.users.order("rating DESC") : User.order("rating DESC")
           build_report_chart if params[:chart]
@@ -15,12 +17,19 @@ module Hatchboy
         private
 
           def build_report_chart
-            scores = UserAvgRating.where(rated_id: @users).group(:date_period, :rated_id).order(:date_period).select('rated_id, date_period, avg(avg_score) AS rating')
-            chart_data = Hash.new
-            scores.group_by(&:date_period).each do |date, date_scores|
-              chart_data[date.to_date.strftime("%B %Y")] = date_scores.map{|s| {id: s.rated_id, name: @users.select{|user| user.id == s.rated_id}.first.name, value: s.rating.round(2)}}
+            scores = UserAvgRating.where(rated_id: @users).select('rated_id, date_period AS g_created_at, avg(avg_score) AS rating').group('g_created_at, rated_id').order('g_created_at')
+            scores = @params[:date] == 'period' ? scores.where(date_period: @params[:period_from]..@params[:period_to]).to_a : scores.to_a
+
+            if scores.count > 0
+              chart_data = group_timeline_from_params scores, {} do |scope, date|
+                @users.collect do |user|
+                  score = scope.select{|s| user.id == s.rated_id}.first if scope
+                  { id: user.id, name: user.name, value: score ? score.rating.round(2) : 0}
+                end
+              end
+              
+              @chart = build_chart({title: "360* ratings", y_title: "Rating value", data: chart_data, without_average: true, columns: true}) if chart_data.length > 0
             end
-            @chart = build_chart({title: "360* ratings", y_title: "Rating value", data: chart_data, without_average: true, columns: true}) if chart_data.length > 0
           end
 
       end
