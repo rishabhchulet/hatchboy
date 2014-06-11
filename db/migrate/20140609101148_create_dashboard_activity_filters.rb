3c7219e403df88146371ee4214a61ebbdc1bb977
aborodavchenko <aborodavchenko@shakuro.com>
2014-06-11 11:39:15 +0400

    Task #15 - Moved new migration to old

--- db/migrate/20140609101148_create_dashboard_activity_filters.rb
+++ db/migrate/20140609101148_create_dashboard_activity_filters.rb
@@ -2,13 +2,13 @@ class CreateDashboardActivityFilters < ActiveRecord::Migration
   def change
     create_table :dashboard_activity_filters do |t|
       t.integer :user_id     
-      t.boolean :users
-      t.boolean :post_receivers
-      t.boolean :payments
-      t.boolean :docu_signs
-      t.boolean :sources
-      t.boolean :teams
-      t.boolean :work_logs
+      t.boolean :users, :default => true
+      t.boolean :post_receivers, :default => true
+      t.boolean :payments, :default => true
+      t.boolean :docu_signs, :default => true
+      t.boolean :sources, :default => true
+      t.boolean :teams, :default => true
+      t.boolean :work_logs, :default => true
     end
     
     add_index :dashboard_activity_filters, :user_id
